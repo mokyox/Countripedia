b2a2c730a3139fa130f016b572f30ad7f238916e
Mo Jama <23492964+mokyox@users.noreply.github.com>
2020-02-25 10:24:34 +0000

    make countrystats type optional

--- src/components/CountryStats/types.ts
+++ src/components/CountryStats/types.ts
@@ -1,16 +1,16 @@
 export interface Props {
-  setSelectedCountry: React.Dispatch<React.SetStateAction<string>>;
-  selectedCountry: {
-    name: string;
-    capital: string;
-    flag: string;
-    population: number;
+  setSelectedCountry?: React.Dispatch<React.SetStateAction<string>>;
+  selectedCountry?: {
+    name?: string;
+    capital?: string;
+    flag?: string;
+    population?: number;
   };
-  country: {
-    name: string;
-    capital: string;
-    flag: string;
-    population: number;
-    languages: [{ name: string }];
+  country?: {
+    name?: string;
+    capital?: string;
+    flag?: string;
+    population?: number;
+    languages?: [{ name: string }];
   };
 }
